432f9e1f5113f87b52833de3bf68c6f496bb79a5
Kvokka <root_p@mail.ru>
2018-02-08 01:59:26 +0000

    Parser test refactoring

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -1,47 +1,49 @@
 require 'test_helper'
 
-describe OnTheBeach::Parser do
+module OnTheBeach
+describe Parser do
   describe 'should parse query' do
     it 'empty test' do
-      assert_equal %i[], OnTheBeach::Parser.()
+      assert_equal %i[], Parser.()
     end
 
     it 'a =>' do
-      assert_equal %i[a], OnTheBeach::Parser.call(a: nil)
-      assert_equal %i[a], OnTheBeach::Parser.call('  a  => ')
+      assert_equal %i[a], Parser.(a: nil)
+      assert_equal %i[a], Parser.('  a  => ')
     end
 
     it "a=> \n b => \n c =>" do
-      assert_equal %i[a b c], OnTheBeach::Parser.call(a: nil, b: nil, c: nil)
-      assert_equal %i[a b c], OnTheBeach::Parser.call("a=>  \n  b => , c =>")
+      assert_equal %i[a b c], Parser.(a: nil, b: nil, c: nil)
+      assert_equal %i[a b c], Parser.("a=>  \n  b => , c =>")
     end
 
     it "a=> \n b => c\n c =>" do
-      assert_equal %i[a c b], OnTheBeach::Parser.call(a: nil, b: :c, c: nil)
-      assert_equal %i[a c b], OnTheBeach::Parser.call("a=> \n b => c\n c =>")
+      assert_equal %i[a c b], Parser.(a: nil, b: :c, c: nil)
+      assert_equal %i[a c b], Parser.("a=> \n b => c\n c =>")
     end
 
     it "a=> \n b => c\n c => f\n d => a\n e => b\n f =>" do
-      assert_equal %i[a d f c b e], OnTheBeach::Parser.call(a: nil, b: :c,  c: :f, d: :a, e: :b, f: nil)
-      assert_equal %i[a d f c b e], OnTheBeach::Parser.call("a=> \n b => c\n c => f\n d => a\n e => b\n f =>")
+      assert_equal %i[a d f c b e], Parser.(a: nil, b: :c,  c: :f, d: :a, e: :b, f: nil)
+      assert_equal %i[a d f c b e], Parser.("a=> \n b => c\n c => f\n d => a\n e => b\n f =>")
     end
       
     it "a=> \n b => c\n c => c" do
       assert_raises ArgumentError do 
-        OnTheBeach::Parser.call("a=> \n b => c\n c => c")
+        Parser.("a=> \n b => c\n c => c")
       end
     end
 
     it "a=> \n b => c\n c => f \n d => a \n e => \n f => b" do
-      assert_raises OnTheBeach::InfiniteLoop do 
-        OnTheBeach::Parser.call("a=> \n b => c\n c => f \n d => a \n e => \n f => b")
+      assert_raises InfiniteLoop do 
+        Parser.("a=> \n b => c\n c => f \n d => a \n e => \n f => b")
       end
     end
 
     it 'return clean result with multiple run' do
-      parser  = OnTheBeach::Parser.new("a=>  \n  b => , c =>")
+      parser  = Parser.new("a=>  \n  b => , c =>")
       parser.()
-      assert_equal %i[a b c], parser.call
+      assert_equal %i[a b c], parser.()
     end
   end
+end
 end
